ae398fa33a9e9fe200517e769528452163de7292
Huy Phan <dachuy@gmail.com>
2012-05-24 15:01:16 +0800

    Update main.m file

---
+++ MacVNKB/main.m
@@ -0,0 +1,14 @@
+//
+//  main.m
+//  MacVNKB
+//
+//  Created by Huy Phan on 20/5/12.
+//  Copyright (c) 2012 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, char *argv[])
+{
+    return NSApplicationMain(argc, (const char **)argv);
+}
